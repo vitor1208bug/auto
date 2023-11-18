679d1f8fd6dc9b9776112cb0ac3ce1e68827ed49
vitor1208bug <131317985+vitor1208bug@users.noreply.github.com>
2023-11-18 10:13:40 -0300

    Create infos.php

---
+++ PHP/infos.php
@@ -0,0 +1,6 @@
+// Configuração do banco de dados
+$host = "concessionaria.cyp6jjti2v9z.sa-east-1.rds.amazonaws.com";
+$username = "jason";
+$password = "ehysuRkvhYqchNpoc2WG";
+$database = "db_conce";
+?>
